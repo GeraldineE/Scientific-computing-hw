4f1b1f50aa1bace18fa4f37cea6ebb05f9e506b7
GeraldineE <geraldine.e.cortes@gmail.com>
2016-03-12 22:30:44 -0500

    taller en cpp

---
+++ Producto.cpp
@@ -0,0 +1,54 @@
+
+#include <stdio.h>
+#include <string>
+using namespace std;
+
+class Producto {
+ private:
+    int idp,cantvoltaje,precio;
+    int numordenlista,tamanio,cantidad,capacidad;
+    string categoria;
+    string nombre;
+
+
+
+
+  public:
+
+   string getShortMessage();
+    void sendShortMessage(string newMessage);
+
+    string getNombre();
+    void setNombre(string nom);
+
+    string getCategoria();
+    void setCategoria(string categ);
+
+    int getCantvoltaje();
+    void setCantvoltaje(int cantv);
+
+    int getPrecio();
+    void setPrecio(int precio);
+
+    string getNumordenlista();
+    void  setNumordenlista(string numorden);
+
+    string getTamanio();
+    void setTamanio(string tam);
+
+    int getCantidad();
+    void setCantidad(int cant);
+
+    int getCapacidad();
+    void setCapacidad(int cap);
+
+    Producto(int idp, string nom, string categ,int cantv,int precio,int numorden, string tam, string cant,int cap); //constructor
+    ~Producto(); //destructor
+};
+
+
+#endif /* Producto_hpp */
+
+
+
+
